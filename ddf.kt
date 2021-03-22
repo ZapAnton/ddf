de2a587bd2e7b55d919d7db0e79a3c057b333fb3
ZapAnton <wc3ft2@gmail.com>
2021-03-22 16:26:39 +0300

    Getting files info from the current directory

--- ddf.kt
+++ ddf.kt
@@ -1,3 +1,13 @@
+import java.io.File
+
+data class FileInfo(val name: String, val extension: String, val size: Double = 0.0)
+
 fun main() {
-    println("Hello World!")
+    val currentDir = File(".")
+    val files = mutableListOf<FileInfo>()
+    currentDir
+        .walkTopDown()
+        .filter { it.isFile() }
+        .forEach { files.add(FileInfo(it.nameWithoutExtension, it.extension, it.length().toDouble())) }
+    files.forEach { println(it) }
 }
